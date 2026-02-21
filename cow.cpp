c807cb610610962390acf638106ea09d2dc6bb04
aryanalikamboh-maker <aryanalikamboh@gmail.com>
2026-02-21 21:21:57 +0500

    First Commit

---
+++ cow.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+    system("color 70");
+    cout<<"        o   ^___^             "<<endl;
+    cout<<"         o  (OO)\\________     "<<endl;
+    cout<<"            (__)\\         )\\//\\ "<<endl;
+    cout<<"                 ||------w |     "<<endl;
+    cout<<"                 ||        || ";
+} 
---
+++ pistol.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"+----------------_ ------------- --------s\n";
+    cout <<" |||   |||||||||| --------- |                o\n";
+    cout <<"     --------------------------------------------\n";
+    cout <<"  ,--------- ----------- --------------\n";
+    cout <<"     /  XXXXXXX   /\n";
+    cout <<"    /  XXXXXXX   /\n"; 
+    cout <<"  /  XXXXXXX   /\n";
+    cout<<"  /  XXXXXXX   /\n";
+    cout<<" ---------------";
+} 
